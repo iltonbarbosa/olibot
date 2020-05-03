df1a70c172bf06ed509921b6f23ea9d5c4b854ec
developersysteminformation <developersysteminformation@gmail.com>
2020-05-03 15:32:16 -0400

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+	],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'class-methods-use-this': 'off',
+		'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'no-useless-escape': 'off',
+		'indent': [2, 'tab'],
+		'semi': ['error', 'never'],
+		'quotes': ['error', 'single'],
+		'no-console': ["error", { allow: ["info", "error"] }]
+	},
 };
--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # frontend-olibot
 
+## Description
+
+O <strong style="color: #2829D0">oli</strong>Bot foi criado para solucionar o problema de comunicação entre os vendedores de marketplace e clientes, reduzindo o tempo de resposta de perguntas mais comuns como: "Tem a cor amarela?" ou "Qual a garantia do produto?".
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
